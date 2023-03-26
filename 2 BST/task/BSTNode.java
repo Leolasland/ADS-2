497568e65a7a4f7f22ce90c25d4b3e44c5e71f2c
Leolasland <hdanyel@student.21-school.ru>
2023-03-26 09:59:49 +0300

    solution bst

--- 2 BST/task/BSTNode.java
+++ 2 BST/task/BSTNode.java
@@ -39,38 +39,141 @@ class BST<T>
 {
   BSTNode<T> Root; // корень дерева, или null
 
+  int count;
+
   public BST(BSTNode<T> node)
   {
     Root = node;
+    count = 1;
   }
 
   public BSTFind<T> FindNodeByKey(int key)
   {
     // ищем в дереве узел и сопутствующую информацию по ключу
-    return null;
+    BSTFind<T> bstFind = new BSTFind<>();
+    if (Root == null) {
+      return bstFind;
+    }
+    BSTNode<T> node = searchNode(Root, key);
+    bstFind.Node = node;
+    bstFind.NodeHasKey = node.NodeKey == key;
+    if (!bstFind.NodeHasKey) {
+      bstFind.ToLeft = (key < node.NodeKey);
+    }
+    return bstFind;
+  }
+
+  private BSTNode<T> searchNode(BSTNode<T> node, int key) {
+    if (key < node.NodeKey && node.LeftChild != null) {
+      return searchNode(node.LeftChild, key);
+    }
+    if (key > node.NodeKey && node.RightChild != null) {
+      return searchNode(node.RightChild, key);
+    }
+    return node;
   }
 
   public boolean AddKeyValue(int key, T val)
   {
     // добавляем ключ-значение в дерево
-    return false; // если ключ уже есть
+    if (Root == null) {
+      Root = new BSTNode<>(key, val, null);
+      return true;
+    }
+    BSTFind<T> bstFind = FindNodeByKey(key);
+    if (bstFind.NodeHasKey) {
+      return false; // если ключ уже есть
+    }
+    BSTNode<T> node = new BSTNode<>(key, val, bstFind.Node);
+    if (bstFind.ToLeft) {
+      bstFind.Node.LeftChild = node;
+    } else {
+      bstFind.Node.RightChild = node;
+    }
+    count++;
+    return true;
   }
 
   public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax)
   {
     // ищем максимальный/минимальный ключ в поддереве
-    return null;
+    if (!FindMax && FromNode.LeftChild != null) {
+      return FinMinMax(FromNode.LeftChild, false);
+    }
+    if (FindMax && FromNode.RightChild != null) {
+      return FinMinMax(FromNode.RightChild, true);
+    }
+    return FromNode;
   }
 
   public boolean DeleteNodeByKey(int key)
   {
     // удаляем узел по ключу
-    return false; // если узел не найден
+    BSTFind<T> bstFind = FindNodeByKey(key);
+    if (!bstFind.NodeHasKey) {
+      return false; // если узел не найден
+    }
+    BSTNode<T> node = bstFind.Node;
+    if (node.LeftChild != null && node.RightChild != null) {
+      BSTNode<T> replace = FinMinMax(node.RightChild, false);
+      if (node.Parent != null && node.Parent.LeftChild == node) {
+        node.Parent.LeftChild = replace;
+      } else if (node.Parent != null && node.Parent.RightChild == node) {
+        node.Parent.RightChild = replace;
+      }
+      replace.Parent = node.Parent;
+      if (node.RightChild != replace) {
+        replace.RightChild = node.RightChild;
+        node.RightChild.Parent = replace;
+      }
+      replace.LeftChild = node.LeftChild;
+      replace.LeftChild.Parent = replace;
+      if (node == Root) {
+        Root = replace;
+      }
+    }
+    else if (node.LeftChild == null && node.RightChild == null) {
+      if (node.Parent != null && node.Parent.LeftChild == node) {
+        node.Parent.LeftChild = null;
+      } else if (node.Parent != null && node.Parent.RightChild == node) {
+        node.Parent.RightChild = null;
+      }
+      if (node == Root) {
+        Root = null;
+      }
+    } else {
+      if (node.RightChild != null) {
+        if (node.Parent != null && node.Parent.LeftChild == node) {
+          node.Parent.LeftChild = node.RightChild;
+        } else if (node.Parent != null && node.Parent.RightChild == node) {
+          node.Parent.RightChild = node.RightChild;
+        }
+        node.RightChild.Parent = node.Parent;
+        if (node == Root) {
+          Root = node.RightChild;
+        }
+      } else {
+        if (node.Parent != null && node.Parent.LeftChild == node) {
+          node.Parent.LeftChild = node.LeftChild;
+        } else if (node.Parent != null && node.Parent.RightChild == node) {
+          node.Parent.RightChild = node.LeftChild;
+        }
+        node.LeftChild.Parent = node.Parent;
+        if (node == Root) {
+          Root = node.LeftChild;
+        }
+      }
+    }
+    count--;
+    return true;
   }
 
   public int Count()
   {
-    return 0; // количество узлов в дереве
+    if (Root == null) {
+      return 0;
+    }
+    return count; // количество узлов в дереве
   }
 
 }
--- 2 BST/task/BSTNodeTest.java
+++ 2 BST/task/BSTNodeTest.java
@@ -6,4 +6,117 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BSTNodeTest {
+
+  @Test
+  void findNodeByKey() {
+    BST<Integer> bst = new BST<>(new BSTNode<Integer>(8, 8, null));
+    bst.AddKeyValue(4, 4);
+    //find present
+    BSTFind<Integer> find = bst.FindNodeByKey(4);
+    assertTrue(find.NodeHasKey);
+    assertEquals(4, find.Node.NodeKey);
+    //find left absent
+    find = bst.FindNodeByKey(2);
+    assertFalse(find.NodeHasKey);
+    assertEquals(bst.Root.LeftChild, find.Node);
+    assertTrue(find.ToLeft);
+    //find right absent
+    find = bst.FindNodeByKey(5);
+    assertFalse(find.NodeHasKey);
+    assertEquals(bst.Root.LeftChild, find.Node);
+    assertFalse(find.ToLeft);
+  }
+
+  @Test
+  void addKeyValue() {
+    BST<Integer> bst = new BST<>(null);
+    assertFalse(bst.FindNodeByKey(8).NodeHasKey);
+    assertNull(bst.Root);
+    assertTrue(bst.AddKeyValue(8, 8));
+    assertEquals(8, bst.Root.NodeKey);
+    //add left child
+    assertFalse(bst.FindNodeByKey(4).NodeHasKey);
+    assertTrue(bst.AddKeyValue(4, 4));
+    assertEquals(4, bst.Root.LeftChild.NodeKey);
+    assertNull(bst.Root.RightChild);
+    assertNull(bst.Root.Parent);
+    //add right child
+    assertFalse(bst.FindNodeByKey(12).NodeHasKey);
+    assertTrue(bst.AddKeyValue(12, 12));
+    assertEquals(12, bst.Root.RightChild.NodeKey);
+    //try to add present
+    assertTrue(bst.FindNodeByKey(4).NodeHasKey);
+    assertFalse(bst.AddKeyValue(4, 4));
+  }
+
+  @Test
+  void finMinMax() {
+    BST<Integer> bst = new BST<>(null);
+    assertTrue(bst.AddKeyValue(8, 8));
+    assertTrue(bst.AddKeyValue(4, 4));
+    assertTrue(bst.AddKeyValue(12, 12));
+    assertTrue(bst.AddKeyValue(2, 2));
+    assertTrue(bst.AddKeyValue(6, 6));
+    assertTrue(bst.AddKeyValue(1, 1));
+    assertTrue(bst.AddKeyValue(3, 3));
+    assertTrue(bst.AddKeyValue(5, 5));
+    assertTrue(bst.AddKeyValue(7, 7));
+
+    assertEquals(12, bst.FinMinMax(bst.Root, true).NodeKey);
+    assertEquals(7, bst.FinMinMax(bst.FindNodeByKey(4).Node, true).NodeKey);
+    assertEquals(3, bst.FinMinMax(bst.FindNodeByKey(2).Node, true).NodeKey);
+
+    assertEquals(1, bst.FinMinMax(bst.Root, false).NodeKey);
+    assertEquals(1, bst.FinMinMax(bst.FindNodeByKey(4).Node, false).NodeKey);
+    assertEquals(5, bst.FinMinMax(bst.FindNodeByKey(6).Node, false).NodeKey);
+  }
+
+  @Test
+  void deleteNodeByKey() {
+    BST<Integer> bst = new BST<>(null);
+    assertTrue(bst.AddKeyValue(8, 8));
+    assertTrue(bst.AddKeyValue(4, 4));
+    assertTrue(bst.AddKeyValue(12, 12));
+    assertTrue(bst.AddKeyValue(2, 2));
+    assertTrue(bst.AddKeyValue(6, 6));
+    assertTrue(bst.AddKeyValue(1, 1));
+    assertTrue(bst.AddKeyValue(3, 3));
+    assertTrue(bst.AddKeyValue(5, 5));
+    assertTrue(bst.AddKeyValue(7, 7));
+
+    assertTrue(bst.FindNodeByKey(2).NodeHasKey);
+    assertTrue(bst.DeleteNodeByKey(2));
+    assertFalse(bst.FindNodeByKey(2).NodeHasKey);
+    assertEquals(3, bst.FindNodeByKey(4).Node.LeftChild.NodeKey);
+    assertEquals(6, bst.FindNodeByKey(4).Node.RightChild.NodeKey);
+    assertNull(bst.FindNodeByKey(3).Node.RightChild);
+    assertEquals(1, bst.FindNodeByKey(3).Node.LeftChild.NodeKey);
+    assertEquals(4, bst.FindNodeByKey(3).Node.Parent.NodeKey);
+    assertEquals(3, bst.FindNodeByKey(1).Node.Parent.NodeKey);
+  }
+
+  @Test
+  void count() {
+    BST<Integer> bst = new BST<>(null);
+    assertEquals(0, bst.Count());
+    assertTrue(bst.AddKeyValue(8, 8));
+    assertEquals(1, bst.Count());
+    assertTrue(bst.AddKeyValue(4, 4));
+    assertTrue(bst.AddKeyValue(12, 12));
+    assertTrue(bst.AddKeyValue(2, 2));
+    assertEquals(4, bst.Count());
+    assertTrue(bst.AddKeyValue(6, 6));
+    assertTrue(bst.AddKeyValue(1, 1));
+    assertTrue(bst.AddKeyValue(3, 3));
+    assertEquals(7, bst.Count());
+    assertTrue(bst.AddKeyValue(5, 5));
+    assertTrue(bst.AddKeyValue(7, 7));
+    assertEquals(9, bst.Count());
+    assertTrue(bst.DeleteNodeByKey(4));
+    assertEquals(8, bst.Count());
+    assertTrue(bst.DeleteNodeByKey(2));
+    assertTrue(bst.DeleteNodeByKey(12));
+    assertTrue(bst.DeleteNodeByKey(3));
+    assertEquals(5, bst.Count());
+  }
 }
