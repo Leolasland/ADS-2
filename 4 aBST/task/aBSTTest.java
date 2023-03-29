20136f2a8768ab9c865ec5f42012fe180334db36
Leolasland <hdanyel@student.21-school.ru>
2023-03-29 23:17:22 +0300

    added aBST

---
+++ 4 aBST/task/aBSTTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class aBSTTest {
+
+  @Test
+  void FindKeyIndex() {
+
+  }
+
+  @Test
+  void AddKey() {
+
+  }
+}
