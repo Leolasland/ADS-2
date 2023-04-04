459adaeaddb03d31e3a0ad1e7cb8990664dc3f6b
Leolasland <hdanyel@student.21-school.ru>
2023-04-04 15:55:27 +0300

    solution GenerateBBSTArray

--- 5 GenerateBBSTArray/task/AlgorithmsDataStructures2.java
+++ 5 GenerateBBSTArray/task/AlgorithmsDataStructures2.java
@@ -12,15 +12,20 @@ public class AlgorithmsDataStructures2
   }
 
   private static int [] fillResult(int [] result, int [] a, int tmp, int left, int right) {
-    int centr = (left + right) / 2;
-    result[tmp] = a[centr];
+    int center = (left + right) / 2;
+    result[tmp] = a[center];
     if (left == right) {
       return result;
     }
-    left = 2 * tmp + 1;
+    int leftChild = 2 * tmp + 1;
+    if (leftChild < result.length && center != (left + right - 1) / 2 && a[(left + right - 1) / 2] != result[0]) {
+      fillResult(result, a, leftChild, left, center - 1);
+    }
+    int rightChild = 2 * tmp + 2;
+    if (rightChild < result.length && center != (center + right + 1) / 2) {
+      fillResult(result, a, rightChild, center + 1, right);
+    }
 
-    right = 2 * tmp + 2;
-
-    return fillResult(result, a, tmp, left, right);
+    return result;
   }
 }
--- 5 GenerateBBSTArray/task/AlgorithmsDataStructures2Test.java
+++ 5 GenerateBBSTArray/task/AlgorithmsDataStructures2Test.java
@@ -8,6 +8,13 @@ public class AlgorithmsDataStructures2Test {
   void generateBBSTArray() {
     int [] a = {50, 25, 75, 23, 37, 62, 84};
     int[] array = AlgorithmsDataStructures2.GenerateBBSTArray(a);
-    System.out.println(array.toString());
+    assertEquals(50, array[0]);
+    assertEquals(25, array[1]);
+    assertEquals(75, array[2]);
+    assertEquals(23, array[3]);
+    assertEquals(37, array[4]);
+    assertEquals(62, array[5]);
+    assertEquals(84, array[6]);
+    assertEquals(a.length, array.length);
   }
 }
