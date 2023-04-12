ad778f1c967df7d8c90870d0374458bac8becd1c
Leolasland <hdanyel@student.21-school.ru>
2023-04-12 22:32:14 +0300

    solution SimpleGraph

--- 8 SimpleGraph/task/SimpleGraph.java
+++ 8 SimpleGraph/task/SimpleGraph.java
@@ -27,6 +27,12 @@ class SimpleGraph
     // ваш код добавления новой вершины
     // с значением value
     // в незанятую позицию vertex
+    for (int i = 0; i < max_vertex; i++) {
+      if (vertex[i] == null) {
+        vertex[i] = new Vertex(value);
+        return;
+      }
+    }
   }
 
   // здесь и далее, параметры v -- индекс вершины
@@ -34,21 +40,42 @@ class SimpleGraph
   public void RemoveVertex(int v)
   {
     // ваш код удаления вершины со всеми её рёбрами
+    if (v < 0 || v > max_vertex) {
+      return;
+    }
+    for (int i = 0; i < max_vertex; i++) {
+      m_adjacency[v][i] = 0;
+      m_adjacency[i][v] = 0;
+    }
+    vertex[v] = null;
   }
 
   public boolean IsEdge(int v1, int v2)
   {
     // true если есть ребро между вершинами v1 и v2
-    return false;
+    if (v1 < 0 || v2 < 0 || v1 > max_vertex || v2 > max_vertex) {
+      return false;
+    }
+    return m_adjacency[v1][v2] == 1 && m_adjacency[v2][v1] == 1;
   }
 
   public void AddEdge(int v1, int v2)
   {
     // добавление ребра между вершинами v1 и v2
+    if (v1 < 0 || v2 < 0 || v1 > max_vertex || v2 > max_vertex) {
+      return;
+    }
+    m_adjacency[v1][v2] = 1;
+    m_adjacency[v2][v1] = 1;
   }
 
   public void RemoveEdge(int v1, int v2)
   {
     // удаление ребра между вершинами v1 и v2
+    if (v1 < 0 || v2 < 0 || v1 > max_vertex || v2 > max_vertex) {
+      return;
+    }
+    m_adjacency[v1][v2] = 0;
+    m_adjacency[v2][v1] = 0;
   }
 }
--- 8 SimpleGraph/task/SimpleGraphTest.java
+++ 8 SimpleGraph/task/SimpleGraphTest.java
@@ -4,4 +4,81 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleGraphTest {
+
+  @Test
+  void addVertex(){
+    SimpleGraph simpleGraph = new SimpleGraph(3);
+    simpleGraph.AddVertex(100);
+    simpleGraph.AddVertex(50);
+    simpleGraph.AddVertex(20);
+    assertEquals(100, simpleGraph.vertex[0].Value);
+    assertEquals(50, simpleGraph.vertex[1].Value);
+    assertEquals(20, simpleGraph.vertex[2].Value);
+    for (int i = 0; i < simpleGraph.m_adjacency.length; i++) {
+      for (int j = 0; j < simpleGraph.m_adjacency.length; j++) {
+        assertEquals(0, simpleGraph.m_adjacency[i][j]);
+      }
+    }
+  }
+
+  @Test
+  void addEdge() {
+    SimpleGraph simpleGraph = new SimpleGraph(3);
+    simpleGraph.AddVertex(100);
+    simpleGraph.AddVertex(50);
+    simpleGraph.AddVertex(20);
+    simpleGraph.AddEdge(0, 1000);
+    simpleGraph.AddEdge(0, 2);
+    assertEquals(1, simpleGraph.m_adjacency[0][2]);
+    assertEquals(1, simpleGraph.m_adjacency[2][0]);
+    assertEquals(0, simpleGraph.m_adjacency[0][1]);
+  }
+
+  @Test
+  void isEdge() {
+    SimpleGraph simpleGraph = new SimpleGraph(3);
+    simpleGraph.AddVertex(100);
+    simpleGraph.AddVertex(50);
+    simpleGraph.AddVertex(20);
+    simpleGraph.AddEdge(0, 2);
+    assertTrue(simpleGraph.IsEdge(0, 2));
+    assertTrue(simpleGraph.IsEdge(2, 0));
+    assertFalse(simpleGraph.IsEdge(0, 1000));
+    assertFalse(simpleGraph.IsEdge(0, 1));
+  }
+
+  @Test
+  void removeEdge() {
+    SimpleGraph simpleGraph = new SimpleGraph(3);
+    simpleGraph.AddVertex(100);
+    simpleGraph.AddVertex(50);
+    simpleGraph.AddVertex(20);
+    simpleGraph.AddEdge(0, 2);
+    assertEquals(1, simpleGraph.m_adjacency[0][2]);
+    assertEquals(1, simpleGraph.m_adjacency[2][0]);
+
+    simpleGraph.RemoveEdge(0, 1000);
+    simpleGraph.RemoveEdge(0, 2);
+    assertEquals(0, simpleGraph.m_adjacency[0][2]);
+    assertEquals(0, simpleGraph.m_adjacency[2][0]);
+  }
+
+  @Test
+  void removeVertex() {
+    SimpleGraph simpleGraph = new SimpleGraph(3);
+    simpleGraph.AddVertex(100);
+    simpleGraph.AddVertex(50);
+    simpleGraph.AddVertex(20);
+    simpleGraph.AddEdge(0, 2);
+    assertEquals(100, simpleGraph.vertex[0].Value);
+    assertEquals(20, simpleGraph.vertex[2].Value);
+    assertEquals(1, simpleGraph.m_adjacency[0][2]);
+    assertEquals(1, simpleGraph.m_adjacency[2][0]);
+
+    simpleGraph.RemoveVertex(100);
+    simpleGraph.RemoveVertex(0);
+    assertEquals(0, simpleGraph.m_adjacency[0][2]);
+    assertEquals(0, simpleGraph.m_adjacency[2][0]);
+    assertNull(simpleGraph.vertex[0]);
+  }
 }
